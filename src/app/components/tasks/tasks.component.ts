b4e01160f426a1812e770ecae2c65579bd99cf53
Narmin Molina <narminmolina@gmail.com>
2023-02-03 21:29:54 -0500

    tasks and jason server

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss']
+})
+export class TasksComponent {
+
+  tasks:Task[]=[];
+
+  constructor(private taskService:TaskService){}
+
+  ngOnInit():void{
+    this.taskService.getTasks().subscribe((tasks)=>this.tasks=tasks);
+  }
+
+}
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { Observable, of } from 'rxjs';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+private apiUrl='http://localhost:3000/tasks'
+  constructor() { }
+
+  getTasks():Observable<Task[]>{
+    const tasks=of(TASKS);
+    return tasks
+  }
+}
